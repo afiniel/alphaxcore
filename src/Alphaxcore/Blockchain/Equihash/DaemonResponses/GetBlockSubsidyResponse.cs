fe861500c27a95e284e4f02effa49fbd37411a56
AlphaX-Projects <77661270+AlphaX-Projects@users.noreply.github.com>
2021-06-25 16:12:43 +0200

    Add ZCash FungStream Payments

--- src/Alphaxcore/Blockchain/Equihash/DaemonResponses/GetBlockSubsidyResponse.cs
+++ src/Alphaxcore/Blockchain/Equihash/DaemonResponses/GetBlockSubsidyResponse.cs
@@ -29,5 +29,16 @@ namespace Alphaxcore.Blockchain.Equihash.DaemonResponses
         public decimal Miner { get; set; }
         public decimal? Founders { get; set; }
         public decimal? Community { get; set; }
+        public List<fundingStreams> FundingStreams { get; set; }
+    }
+    
+    public class fundingStreams
+    {
+        public string Recipient { get; set; }
+        public string Specification { get; set; }
+        public decimal Value { get; set; }
+        public decimal ValueZat { get; set; }
+        public string Address { get; set; }
+
     }
 }
